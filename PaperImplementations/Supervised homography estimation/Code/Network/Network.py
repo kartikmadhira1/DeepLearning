1ac25d39ba44f90b6bd855bebfabd0d9568e3c0a
Kartik Madhira <kartikmadhira1@gmail.com>
2019-03-04 20:12:56 -0500

    added folders

---
+++ PaperImplementations/Supervised homography estimation/Code/Network/Network.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 11:00:39 2019
+@author: kartikmadhira
+"""
+
+
+import tensorflow as tf
+import sys
+import numpy as np
+# Don't generate pyc codes
+sys.dont_write_bytecode = True
+
+def HomographyModel(Img, ImageSize, MiniBatchSize):
+    """
+    Inputs: 
+    Img is a MiniBatch of the current image
+    ImageSize - Size of the Image
+    Outputs:
+    prLogits - logits output of the network
+    prSoftMax - softmax output of the network
+    """
+
+    conv1=tf.layers.conv2d(Img,filters=64,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    norm1 = tf.layers.batch_normalization(conv1)
+    
+    
+    conv2=tf.layers.conv2d(norm1,
+                           filters=64,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    norm2 = tf.layers.batch_normalization(conv2)
+    
+    pool2=tf.layers.max_pooling2d(inputs=norm2,
+                                  pool_size=(2,2),
+                                  strides=2)
+    
+    #drop1=tf.layers.dropout(pool2,rate=0.5)
+
+    conv3=tf.layers.conv2d(pool2,
+                           filters=64,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    norm3 = tf.layers.batch_normalization(conv3)
+    
+    conv4=tf.layers.conv2d(norm3,
+                           filters=64,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    
+    pool3=tf.layers.max_pooling2d(inputs=conv4,
+                                  pool_size=(2,2),
+                                  strides=2)
+    
+    #drop2=tf.layers.dropout(pool3,rate=0.5)
+    
+    conv5=tf.layers.conv2d(pool3,
+                           filters=128,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    norm4 = tf.layers.batch_normalization(conv5)
+
+#    #pool4=tf.layers.max_pooling2d(inputs=norm4,
+#                                  pool_size=(2,2),
+#                                  strides=2)
+    
+    conv6=tf.layers.conv2d(norm4,
+                           filters=128,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    norm5 = tf.layers.batch_normalization(conv6)
+
+    pool4=tf.layers.max_pooling2d(inputs=norm5,
+                                  pool_size=(2,2),
+                                  strides=2)
+    
+    conv7=tf.layers.conv2d(pool4,
+                           filters=128,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    
+    #drop3=tf.layers.dropout(pool4,rate=0.5)
+    norm6 = tf.layers.batch_normalization(conv7)
+    
+    conv8=tf.layers.conv2d(norm6,
+                           filters=128,
+                           kernel_size=(3,3),
+                           padding="same",
+                           activation=tf.nn.relu)
+    
+    #now flattening the layer to add a fully connected layer
+    flatLayer1=tf.reshape(conv8,[-1,conv8.shape[1:4].num_elements()])
+    
+    #adding a dense layer
+    dense1=tf.layers.dense(inputs=flatLayer1,units=1024,activation=tf.nn.relu)
+    
+    #add dropout if required!
+    H4Pt=tf.layers.dense(dense1,units=8,activation=None)
+    
+    return H4Pt
+
---
+++ PaperImplementations/Supervised homography estimation/dataGather.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import os 
+import time 
+
+#Parameters set:
+cropSize=128
+#range for perturbing the corners to get the homographies [-rho,+rho]
+rho=16
+#resize shape
+resize=(320,240)
+#number of train sets to be made
+numTrainData=82780
+#file path
+filePath="/home/kartikmadhira/datasets/ms-coco/train2014/"
+saveDest="/home/kartikmadhira/datasets/ms-coco/"
+
+def loadTrainList(filePath,numTrainData):
+    fileList=os.listdir(filePath)
+    dataList=[]
+    #take any random number and take 5000 of these images
+    for i in range(numTrainData):
+        dataList.append(filePath+fileList[i])
+    return dataList
+
+
+def saveData(savePath,data,i):
+	if not os.path.exists(savePath+'data'):
+	    os.makedirs(savePath+'data')
+	if not os.path.exists(savePath+'labels'):
+	    os.makedirs(savePath+'labels')
+	np.savez(savePath+'data/'+str(i)+'.npz',data[0])
+	np.savez(savePath+'labels/'+str(i)+'.npz',data[1])
+
+
+def getImages(dataList,numTrainData,saveDest):
+    #load image
+    for i in range(numTrainData):
+        
+        image=cv2.imread(dataList[i],cv2.IMREAD_GRAYSCALE)
+        image=cv2.resize(image,resize)
+        #get a random x and y location that does not have the borders
+        #x is Y and y is X!
+        getLocX=random.randint(105,160)
+        getLocY=random.randint(105,225)
+        #crop the image
+        patchA=image[getLocX-int(cropSize/2):getLocX+int(cropSize/2),getLocY-int(cropSize/2):getLocY+int(cropSize/2)]
+
+        #perturb image randomly and apply homography
+        pts1=np.float32([[getLocY-cropSize/2+random.randint(-rho,rho),getLocX-cropSize/2+random.randint(-rho,rho)],
+              [getLocY+cropSize/2+random.randint(-rho,rho),getLocX-cropSize/2+random.randint(-rho,rho)],
+              [getLocY+cropSize/2+random.randint(-rho,rho),getLocX+cropSize/2+random.randint(-rho,rho)],
+              [getLocY-cropSize/2+random.randint(-rho,rho),getLocX+cropSize/2+random.randint(-rho,rho)]])
+        pts2=np.float32([[getLocY-cropSize/2,getLocX-cropSize/2],
+              [getLocY+cropSize/2,getLocX-cropSize/2],
+              [getLocY+cropSize/2,getLocX+cropSize/2],
+              [getLocY-cropSize/2,getLocX+cropSize/2]])
+
+        H4pts=pts2-pts1
+    
+        #get the perspective transform
+        hAB=cv2.getPerspectiveTransform(pts2,pts1)
+        #get the inverse
+        hBA=np.linalg.inv(hAB)
+        #get the warped image from the inverse homography generated in the dataset
+        warped=np.asarray(cv2.warpPerspective(image,hBA,resize)).astype(np.uint8)
+        #get the last patchB at the same location but on the warped image.
+        patchB=warped[getLocX-int(cropSize/2):getLocX+int(cropSize/2),getLocY-int(cropSize/2):getLocY+int(cropSize/2)]
+        #stack images on top of each other.
+        stackedData=np.dstack((patchA,patchB))
+        # #homogrpahy check
+        # orig=cv2.warpPerspective(patchB,hAB,(128,128))
+        # plt.subplot(1,2,1)
+        # plt.imshow(patchA)
+        # plt.subplot(1,2,2)
+        # plt.imshow(patchB)
+        if(i%3000==0):
+            print('Saved '+str(i)+' images')
+        saveData(saveDest,[stackedData,H4pts],i)
+
+#get the files
+
+files=loadTrainList(filePath,numTrainData)
+getImages(files,numTrainData,saveDest)
